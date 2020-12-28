5811a9e9be20a7c37025a2bbe9513b0054fbc68c
Hank <guanhanchen6@gmail.com>
2020-12-28 14:45:51 +0800

    本地database

---
+++ 查詢.py
@@ -0,0 +1,9 @@
+total = []
+
+food = ["奶茶","提拉米蘇","拉麵","牛排",]
+toy = ["Bear","PS2","Gameboy"]
+
+total.append(food)
+total.append(toy)
+
+print(total)
---
+++ 清單.py
@@ -0,0 +1,4 @@
+
+
+list1 = [["牛奶","蘋果","咖啡"],["Bear","Gameboy","Xbox"]]
+print(list1)
